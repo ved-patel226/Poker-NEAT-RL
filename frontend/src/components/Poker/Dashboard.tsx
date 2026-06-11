4911870dd258804e714fd48ba86dc57ba72b84b6
Ved Patel <talk2ved11@gmail.com>
2026-06-11 16:42:00 -0400

    Initial commit

--- frontend/src/components/Poker/Dashboard.tsx
+++ frontend/src/components/Poker/Dashboard.tsx
@@ -3,10 +3,10 @@ export default function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
-            <p>Here, you can start games with your poker ai!</p>
+            <p>Here, you can start games with your poker AI!</p>
 
             <div>
-                <h1></h1>
+                <a>Start</a>
             </div>
         </div>
     )
